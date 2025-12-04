a6182f345976bb1abce6506ae19e71d7a45a9c43
aarav <aaravpradhan12365@gmail.com>
2025-12-04 12:26:10 +0545

    Feat: fixed

--- src/app.test.js
+++ src/app.test.js
@@ -9,7 +9,7 @@ describe('API Tests', () => {
     test('GET / should return welcome message', async () => {
         const response = await request(app).get('/');
         expect(response.statusCode).toBe(200);
-        expect(response.body.message).toBe('Hello, CI/CD World!');
+        expect(response.body.message).toBe('Hello, CI/CD World! ssh and docker');
     });
 
     test('GET /goodbye should return goodbye message', async () => {
